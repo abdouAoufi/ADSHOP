2dc5c4a56f9170306e88672619f95ebbddb681ae
Abderhamnae Aoufi <abdouou7@gmail.com>
2020-10-09 17:36:47 +0100

    first commit initalize project

---
+++ app.js
@@ -0,0 +1,17 @@
+var list = ['this '];
+/*
+Zen mode for full screen
+ctrl + b hile project bar 
+ctrl + b  close file
+ctrl + d select word
+ctrl + shift + tab switch between files
+<!-- TODO:this should be a to do comment -->
+<!-- **:this should be a to do comment -->
+<!--!this is danger-->
+*/
+
+var functionObjetct = function() {
+    var Age = 16;
+}
+
+console.log('hello world ')
---
+++ qo.js
@@ -0,0 +1,8 @@
+console.log('hello world');
+
+function getAge(bornYear) {
+    var age = 2020 - bornYear;
+    console.log(age);
+}
+
+getAge(1989);
